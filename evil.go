d00aad921abd1d3b1bdf766b35158164ba594511
christopherdumas <christopherdumas@gmail.com>
2016-09-16 20:16:15 -0700

    Reached the level of something like nano

--- evil.go
+++ evil.go
@@ -6,12 +6,16 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"unicode/utf8"
 
 	"github.com/jroimartin/gocui"
+	"github.com/mgutz/ansi"
 )
 
+var currentFileName string
+
 func nextView(g *gocui.Gui, v *gocui.View) error {
-	if v == nil || v.Name() == "side" {
+	if v == nil || v.Name() == "side" || v.Name() == "info" {
 		return g.SetCurrentView("main")
 	}
 	return g.SetCurrentView("side")
@@ -43,34 +47,32 @@ func cursorUp(g *gocui.Gui, v *gocui.View) error {
 	return nil
 }
 
-func changeFile(fileName *string) func(g *gocui.Gui, v *gocui.View) error {
-	return func(g *gocui.Gui, v *gocui.View) error {
-		var l string
-		var err error
+func changeFile(g *gocui.Gui, v *gocui.View) error {
+	var l string
+	var err error
 
-		_, cy := v.Cursor()
-		if l, err = v.Line(cy); err != nil {
-			l = ""
-		}
-
-		*fileName = l
-
-		mv, err := g.View("main")
-		if err != nil {
-			panic(err)
-		}
-
-		mv.Clear()
-		if err := mv.SetOrigin(0, 0); err != nil {
-			return err
-		}
-		if err := mv.SetCursor(0, 0); err != nil {
-			return err
-		}
-		loadFile(*fileName, g, mv)
-
-		return nil
+	_, cy := v.Cursor()
+	if l, err = v.Line(cy); err != nil {
+		l = ""
 	}
+
+	currentFileName = l
+
+	mv, err := g.View("main")
+	if err != nil {
+		panic(err)
+	}
+
+	mv.Clear()
+	if err := mv.SetOrigin(0, 0); err != nil {
+		return err
+	}
+	if err := mv.SetCursor(0, 0); err != nil {
+		return err
+	}
+	loadFile(currentFileName, g, mv)
+
+	return nil
 }
 
 func quit(g *gocui.Gui, v *gocui.View) error {
@@ -78,7 +80,6 @@ func quit(g *gocui.Gui, v *gocui.View) error {
 }
 
 func keybindings(g *gocui.Gui) error {
-	currentFileName := os.Args[1]
 	if err := g.SetKeybinding("side", gocui.KeyCtrlSpace, gocui.ModNone, nextView); err != nil {
 		return err
 	}
@@ -94,41 +95,39 @@ func keybindings(g *gocui.Gui) error {
 	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding("side", gocui.KeyEnter, gocui.ModNone, changeFile(&currentFileName)); err != nil {
+	if err := g.SetKeybinding("side", gocui.KeyEnter, gocui.ModNone, changeFile); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding("main", gocui.KeyCtrlS, gocui.ModNone, saveMain(currentFileName)); err != nil {
+	if err := g.SetKeybinding("main", gocui.KeyCtrlS, gocui.ModNone, saveMain); err != nil {
 		return err
 	}
 	return nil
 }
 
-func saveMain(fileName string) func(g *gocui.Gui, v *gocui.View) error {
-	return func(g *gocui.Gui, v *gocui.View) error {
-		f, err := os.OpenFile(fileName, os.O_WRONLY, 0777)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
+func saveMain(g *gocui.Gui, v *gocui.View) error {
+	f, err := os.OpenFile(currentFileName, os.O_WRONLY, 0777)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
 
-		p := make([]byte, 5)
-		v.Rewind()
-		for {
-			n, err := v.Read(p)
-			if n > 0 {
-				if _, err := f.Write(p[:n]); err != nil {
-					return err
-				}
-			}
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
+	p := make([]byte, 5)
+	v.Rewind()
+	for {
+		n, err := v.Read(p)
+		if n > 0 {
+			if _, err := f.Write(p[:n]); err != nil {
 				return err
 			}
 		}
-		return nil
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
 func loadFile(fileName string, g *gocui.Gui, v *gocui.View) error {
@@ -139,6 +138,7 @@ func loadFile(fileName string, g *gocui.Gui, v *gocui.View) error {
 	fmt.Fprintf(v, "%s", b)
 	v.Editable = true
 	v.Wrap = true
+	v.Title = fileName
 	if err := g.SetCurrentView("main"); err != nil {
 		return err
 	}
@@ -148,7 +148,7 @@ func loadFile(fileName string, g *gocui.Gui, v *gocui.View) error {
 
 func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("side", -1, -1, 30, maxY); err != nil {
+	if v, err := g.SetView("side", -1, 0, 27, maxY); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -158,17 +158,169 @@ func layout(g *gocui.Gui) error {
 			fmt.Fprintln(v, f.Name())
 		}
 	}
-	if v, err := g.SetView("main", 30, -1, maxX, maxY); err != nil {
-		v.Title = os.Args[1]
+	if v, err := g.SetView("main", 27, 0, maxX, maxY-2); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
+		v.Title = os.Args[1]
 		loadFile(os.Args[1], g, v)
+		g.Editor = gocui.EditorFunc(simpleEditor(g))
+	}
+	if v, err := g.SetView("info", 27, maxY-2, maxX, maxY); err != nil {
+		v.Editable = true
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		mv, err := g.View("main")
+		if err != nil {
+			return err
+		}
+		cx, cy := mv.Cursor()
+		var mode string
+		if g.Mouse {
+			mode = "INSERT"
+		} else {
+			mode = "NORMAL"
+		}
+		_, oy := v.Origin()
+		v.Title = fmt.Sprintf("%s─────────────────────────────────L%d/C%d", mode, cy+oy, cx)
 	}
 	return nil
 }
 
+func moveCursor(g *gocui.Gui, v *gocui.View, fun func(x, y int) (int, int)) {
+	x, y := v.Cursor()
+	nx, ny := fun(x, y)
+	var l string
+	var err error
+	if l, err = v.Line(ny); err != nil {
+		l = ""
+	}
+	if nx > utf8.RuneCountInString(l) {
+		nx, ny = 0, ny
+	} else if nx < 0 {
+		nx, ny = 0, ny
+	}
+	if err := v.SetCursor(nx, ny); err != nil {
+		ox, oy := v.Origin()
+		dy := 0
+		if ny > y {
+			dy = 1
+		} else if ny < y && oy-1 >= 0 {
+			dy = -1
+		}
+		if err := v.SetOrigin(ox, oy+dy); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func switchToCommand(g *gocui.Gui, v *gocui.View) error {
+	err := g.SetCurrentView("info")
+	if err != nil {
+		return err
+	}
+	iv, err := g.View("info")
+	if err != nil {
+		return err
+	}
+	iv.Clear()
+	if err := v.SetCursor(4, 0); err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func runCommand(g *gocui.Gui, v *gocui.View) {
+	v.Clear()
+	switch v.ViewBuffer() {
+	case "w":
+		saveMain(g, v)
+	default:
+		v.Clear()
+		if err := v.SetCursor(0, 0); err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(v, ansi.Color("Bad command: %s", "cyan+b+h"), v.ViewBuffer())
+	}
+}
+
+func simpleEditor(g *gocui.Gui) func(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
+	return func(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
+		if v.Name() == "main" {
+			switch {
+			case ch == 'i' && !g.Mouse:
+				g.Mouse = true
+			case ch == ':' && !g.Mouse:
+				switchToCommand(g, v)
+			case (ch == 'j' && !g.Mouse) || key == gocui.KeyArrowDown:
+				moveCursor(g, v, func(x, y int) (int, int) {
+					return x, y + 1
+				})
+			case (ch == 'k' && !g.Mouse) || key == gocui.KeyArrowUp:
+				moveCursor(g, v, func(x, y int) (int, int) {
+					return x, y - 1
+				})
+			case (ch == 'l' && !g.Mouse) || key == gocui.KeyArrowRight:
+				moveCursor(g, v, func(x, y int) (int, int) {
+					return x + 1, y
+				})
+			case (ch == 'h' && !g.Mouse) || key == gocui.KeyArrowLeft:
+				moveCursor(g, v, func(x, y int) (int, int) {
+					return x - 1, y
+				})
+			case ch != 0 && mod == 0 && g.Mouse:
+				v.EditWrite(ch)
+			case key == gocui.KeySpace && g.Mouse:
+				v.EditWrite(' ')
+			case (key == gocui.KeyBackspace || key == gocui.KeyBackspace2) && g.Mouse:
+				v.EditDelete(true)
+			case key == gocui.KeyBackspace || key == gocui.KeyBackspace2:
+				moveCursor(g, v, func(x, y int) (int, int) {
+					return x - 1, y
+				})
+			case key == gocui.KeyEsc:
+				g.Mouse = false
+				moveCursor(g, v, func(x, y int) (int, int) {
+					return x - 1, y
+				})
+			}
+			iv, err := g.View("info")
+			if err != nil {
+				panic(err)
+			}
+			mv, err := g.View("main")
+			if err != nil {
+				panic(err)
+			}
+			cx, cy := mv.Cursor()
+			_, oy := v.Origin()
+			var mode string
+			if g.Mouse {
+				mode = "INSERT"
+				iv.Title = fmt.Sprintf("%s─────────────────────────────────L%d/C%d", mode, oy+cy, cx)
+			} else {
+				mode = "NORMAL"
+				iv.Title = fmt.Sprintf("%s─────────────────────────────────L%d/C%d", mode, oy+cy, cx)
+			}
+		} else if v.Name() == "info" {
+			switch {
+			case ch != 0 && mod == 0:
+				v.EditWrite(ch)
+			case key == gocui.KeySpace:
+				v.EditWrite(' ')
+			case key == gocui.KeyBackspace || key == gocui.KeyBackspace2:
+				v.EditDelete(true)
+			case key == gocui.KeyEnter:
+				runCommand(g, v)
+				nextView(g, v)
+			}
+		}
+	}
+}
+
 func main() {
+	currentFileName = os.Args[1]
 	g := gocui.NewGui()
 	if err := g.Init(); err != nil {
 		log.Panicln(err)
@@ -182,6 +334,7 @@ func main() {
 	g.SelBgColor = gocui.ColorGreen
 	g.SelFgColor = gocui.ColorBlack
 	g.Cursor = true
+	g.InputEsc = true
 
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
 		log.Panicln(err)
